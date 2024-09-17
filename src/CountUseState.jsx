55b5be012c778045c0c2bdcc55484c9352619787
fishagbalo <fishagbalo@gmail.com>
2024-09-17 13:03:35 +0800

    studiying useState

--- src/CountUseState.jsx
+++ src/CountUseState.jsx
@@ -1,15 +1,39 @@
 import React, { useState } from 'react'
 
-function CountUseState() {
-    const [count, setCount] = useState(0)
+function countInitial() {
+  console.log("run function")
+  return 0
+}
 
-    const increaseCount = () => {
-        setCount(count +1)
-    }
+function CountUseState() {
+  
+  // const [count, setCount] = useState(() => countInitial()) 
+  // Function Version that will run in the first state
+
+
+  // const [state, setState] = useState({count: 4, theme: 'blue'}) 
+  // const count = state.count
+  // const theme = state.theme
+
+  const [count, setCount] = useState(4)
+  const [theme, setTheme] = useState("Blue")
+
+  
+  function increment(){
+    setCount(prevCount => prevCount +1 )
+  }
+  // To update all the data in object
+
+  function decrement(){
+    setCount(prevCount => prevCount - 1 )
+  }
+ 
   return (
     <div>
-        <h1>Count: {count}</h1>
-        <button onClick={increaseCount}>Increase</button>
+      <button onClick={decrement}>-</button>
+       <span>{count}</span>
+       <span>{theme}</span>
+       <button onClick={increment}>+</button>
     </div>
   )
 }
